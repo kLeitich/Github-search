f39232691b14be5f3f12b4f4cd7a32fedaf7554c
kLeitich <kevleitich@gmail.com>
2022-04-18 20:02:24 +0300

    adding git search component to the repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitsearchComponent } from './gitsearch/gitsearch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitsearchComponent
   ],
   imports: [
     BrowserModule,
